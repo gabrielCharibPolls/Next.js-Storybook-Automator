4541ab7356170f3af39dd0d8dc7303b018d15488
Gabriel Charib <gabrielcharib@gmail.com>
2023-02-28 10:32:31 +0100

    basic code for depe lerning
    ///////////////////////////////////////////////////////////////////
    todo:
    
    see how I'm going to provide him with csv files to make him learn the basic structure of a next.js / react project
    
    understand to install storybooks / best practices
    
    how to test smart components
    ///////////////////////////////////////////////////////////
    for recall the good practice is to use a folder UI or this finds all the component and it is there or one installs storybooks
    ///////////////////////////////////////////////////////////

--- automate_stories.py
+++ automate_stories.py
@@ -9,6 +9,66 @@ import subprocess
 # ou faire en sorte qu'il escane tout les repertotire et 
 # sous_repertoire pour trouver  les composants 
 ######################################################################
+
+
+
+
+
+import os
+
+# Récupère la liste de tous les fichiers .tsx dans le dossier components
+files = os.listdir("components")
+tsx_files = [file for file in files if file.endswith(".tsx")]
+
+# Parcours chaque fichier .tsx et crée un fichier story pour chaque composant
+for file in tsx_files:
+    # Récupère le nom du composant en retirant l'extension .tsx
+    component_name = file.replace(".tsx", "")
+    
+    # Écrit le contenu du fichier story pour le composant
+    with open(f"stories/{component_name}.stories.tsx", "w") as f:
+        f.write(f"import React from 'react';\n")
+        f.write(f"import {component_name} from '../components/{component_name}';\n")
+        f.write(f"\n")
+        f.write(f"export default {{\n")
+        f.write(f"  title: '{component_name}',\n")
+        f.write(f"  component: {component_name},\n")
+        f.write(f"}};\n")
+
+############################################################################
+################code depe lerning #########################################
+############################################################################
+import tensorflow as tf
+
+# Charger les données d'entraînement
+train_data = [...] # Les données d'entraînement
+train_labels = [...] # Les labels d'entraînement
+
+# Définir le modèle
+model = tf.keras.Sequential([
+    tf.keras.layers.Dense(128, activation='relu', input_shape=(len(train_data[0]),)),
+    tf.keras.layers.Dropout(0.5),
+    tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Dropout(0.5),
+    tf.keras.layers.Dense(len(train_labels[0]), activation='softmax')
+])
+
+# Compiler le modèle
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+# Entraîner le modèle
+model.fit(train_data, train_labels, epochs=50, batch_size=32)
+
+# Générer des prédictions
+predictions = model.predict([...]) # Les données de test
+
+# Afficher les prédictions
+print(predictions)
+########################################################################
+
+
+
+
 COMPONENTS_PATH = "path/to/nextjs/components"
 ######################################################################
 # Chemin vers le dossier de sortie pour les stories
