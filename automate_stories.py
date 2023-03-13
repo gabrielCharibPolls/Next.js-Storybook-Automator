5aa4ca2b12d718ffe1372f524833599f0be1f92e
Gabriel Charib <gabrielcharib@gmail.com>
2023-03-13 10:55:48 +0100

    add a method that will search through all in the project a folder called components or /Ui
    todo :
    ###################################################
    for a next update the ideal would be to find a way to do it more genreic in the first time the first letter for example compos********
    in the idea if a Spanish or Italian uses my script
    
    later analyze the component files and manage to find them and move them to a folder and do all this by updating the imports
    #####################################################

--- automate_stories.py
+++ automate_stories.py
@@ -14,120 +14,160 @@ import json
 
 
 #########################################################################
-## etpe 1 : verifier si storybook est installer dans le project 
+## etape 1 : verifier si storybook est installer dans le project 
 ## si c'est pas le cas il installe via la commande 
 #########################################################################
 
 
-
+##########################################################################
 # Chemin vers le fichier package.json 
-package_file_path = "chemin/vers/package.json"
+package_file_path = "package.json"
 
+###########################################################################
 # Charger le contenu du fichier package.json
 with open(package_file_path) as package_file:
     package_data = json.load(package_file)
 
 # Vérifier si la dépendance Storybook est présente
 if "@storybook/react" not in package_data["dependencies"]:
-    # Installer la dépendance Storybook
-    subprocess.run(["npm", "install", "--save-dev", "@storybook/react"])
+    # Demander à l'utilisateur s'il souhaite installer Storybook
+    install_storybook = input("Storybook n'est pas installé. Voulez-vous l'installer ? (y/n) ")
+
+    if install_storybook.lower() == "y":
+        # Demander à l'utilisateur le chemin du répertoire d'installation
+        install_path = input("Entrez le chemin du répertoire d'installation de Storybook : ")
+
+        # Installer la dépendance Storybook dans le répertoire spécifié
+        subprocess.run(["npm", "install", "--save-dev" ], cwd=install_path)
+        print("Storybook a été installé avec succès dans le répertoire :", install_path)
+    else:
+        print("Storybook n'a pas été installé.")
+else:
+    print("Storybook est déjà installé.")
+
+###########################################################################################################
+# etape 2 : rechercher dans tout les dossiers et sous-dossiers un dossier qui s'appel components ou ui 
+#########################################################################################################
+
+
+import os
+
+def find_components_folder():
+    """
+    Fonction qui recherche le dossier "components" ou "ui" dans un projet Next.js
+    à partir du répertoire en cours.
+
+    :return: le chemin complet du dossier "components" ou "ui" s'il est trouvé, None sinon
+    """
+    for root, dirs, files in os.walk('.'):
+        if "components" in dirs:
+            return os.path.join(root, "components")
+        elif "ui" in dirs:
+            return os.path.join(root, "ui")
+    return None
+
+components_folder_path = find_components_folder()
+if components_folder_path is not None:
+    print(f"Le dossier 'components' ou 'ui' se trouve ici : {components_folder_path}")
+else:
+    print("Le dossier 'components' ou 'ui' n'a pas été trouvé.")
 
 
 
 
 
-
+#########################################################################
 # Récupère la liste de tous les fichiers .tsx dans le dossier components
-files = os.listdir("components")
-tsx_files = [file for file in files if file.endswith(".tsx")]
-
-# Parcours chaque fichier .tsx et crée un fichier story pour chaque composant
-for file in tsx_files:
-    # Récupère le nom du composant en retirant l'extension .tsx
-    component_name = file.replace(".tsx", "")
+# files = os.listdir("components")
+# tsx_files = [file for file in files if file.endswith(".tsx")]
+# ########################################################################################
+# # Parcours chaque fichier .tsx et crée un fichier story pour chaque composant
+# for file in tsx_files:
+#     # Récupère le nom du composant en retirant l'extension .tsx
+#     component_name = file.replace(".tsx", "")
     
-    # Écrit le contenu du fichier story pour le composant
-    with open(f"stories/{component_name}.stories.tsx", "w") as f:
-        f.write(f"import React from 'react';\n")
-        f.write(f"import {component_name} from '../components/{component_name}';\n")
-        f.write(f"\n")
-        f.write(f"export default {{\n")
-        f.write(f"  title: '{component_name}',\n")
-        f.write(f"  component: {component_name},\n")
-        f.write(f"}};\n")
+#     # Écrit le contenu du fichier story pour le composant
+#     with open(f"stories/{component_name}.stories.tsx", "w") as f:
+#         f.write(f"import React from 'react';\n")
+#         f.write(f"import {component_name} from '../components/{component_name}';\n")
+#         f.write(f"\n")
+#         f.write(f"export default {{\n")
+#         f.write(f"  title: '{component_name}',\n")
+#         f.write(f"  component: {component_name},\n")
+#         f.write(f"}};\n")
 
 ############################################################################
 ################code depe lerning #########################################
 ############################################################################
 
 
-# Charger les données d'entraînement
-train_data = [...] # Les données d'entraînement
-train_labels = [...] # Les labels d'entraînement
+# # Charger les données d'entraînement
+# train_data = [...] # Les données d'entraînement
+# train_labels = [...] # Les labels d'entraînement
 
-# Définir le modèle
-model = tf.keras.Sequential([
-    tf.keras.layers.Dense(128, activation='relu', input_shape=(len(train_data[0]),)),
-    tf.keras.layers.Dropout(0.5),
-    tf.keras.layers.Dense(64, activation='relu'),
-    tf.keras.layers.Dropout(0.5),
-    tf.keras.layers.Dense(len(train_labels[0]), activation='softmax')
-])
+# # Définir le modèle
+# model = tf.keras.Sequential([
+#     tf.keras.layers.Dense(128, activation='relu', input_shape=(len(train_data[0]),)),
+#     tf.keras.layers.Dropout(0.5),
+#     tf.keras.layers.Dense(64, activation='relu'),
+#     tf.keras.layers.Dropout(0.5),
+#     tf.keras.layers.Dense(len(train_labels[0]), activation='softmax')
+# ])
 
-# Compiler le modèle
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+# # Compiler le modèle
+# model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-# Entraîner le modèle
-model.fit(train_data, train_labels, epochs=50, batch_size=32)
+# # Entraîner le modèle
+# model.fit(train_data, train_labels, epochs=50, batch_size=32)
 
-# Générer des prédictions
-predictions = model.predict([...]) # Les données de test
+# # Générer des prédictions
+# predictions = model.predict([...]) # Les données de test
 
-# Afficher les prédictions
-print(predictions)
+# # Afficher les prédictions
+# print(predictions)
 ########################################################################
 
 
 
 
-COMPONENTS_PATH = "path/to/nextjs/components"
-######################################################################
-# Chemin vers le dossier de sortie pour les stories
-######################################################################
-STORIES_PATH = "path/to/output/stories"
-######################################################################
-# Regex pour extraire le nom du composant à partir du nom de fichier
-COMPONENT_NAME_REGEX = r"^(.*?)\.js$"
-######################################################################
-# Supprimer le dossier de sortie s'il existe déjà
-######################################################################
-if os.path.exists(STORIES_PATH):
-    shutil.rmtree(STORIES_PATH)
-######################################################################
-# Créer le dossier de sortie
-######################################################################
-os.mkdir(STORIES_PATH)
-######################################################################
-# Parcourir tous les fichiers du dossier des composants
-######################################################################
-for filename in os.listdir(COMPONENTS_PATH):
-    ######################################################################
-    # Vérifier si le fichier est un fichier JavaScript 
-    # faire en sorte que ça marche aussi pour type script 
-    # dans lideal pas juste pour react / next.js 
-    # mais les autres framwork 
-    ######################################################################
-    if filename.endswith(".js"):
-        ######################################################################
-        # Extraire le nom du composant à partir du nom de fichier
-        ######################################################################
-        component_name = re.match(COMPONENT_NAME_REGEX, filename).group(1)
-        subprocess.run(
-            f"npx storycap {os.path.join(COMPONENTS_PATH, filename)} --out {os.path.join(STORIES_PATH, component_name)}.stories.js",
-            shell=True,
-            check=True
-        )
-print("Toutes les stories ont été générées avec succès !")
+# COMPONENTS_PATH = "path/to/nextjs/components"
+# ######################################################################
+# # Chemin vers le dossier de sortie pour les stories
+# ######################################################################
+# STORIES_PATH = "path/to/output/stories"
+# ######################################################################
+# # Regex pour extraire le nom du composant à partir du nom de fichier
+# COMPONENT_NAME_REGEX = r"^(.*?)\.js$"
+# ######################################################################
+# # Supprimer le dossier de sortie s'il existe déjà
+# ######################################################################
+# if os.path.exists(STORIES_PATH):
+#     shutil.rmtree(STORIES_PATH)
+# ######################################################################
+# # Créer le dossier de sortie
+# ######################################################################
+# os.mkdir(STORIES_PATH)
+# ######################################################################
+# # Parcourir tous les fichiers du dossier des composants
+# ######################################################################
+# for filename in os.listdir(COMPONENTS_PATH):
+#     ######################################################################
+#     # Vérifier si le fichier est un fichier JavaScript 
+#     # faire en sorte que ça marche aussi pour type script 
+#     # dans lideal pas juste pour react / next.js 
+#     # mais les autres framwork 
+#     ######################################################################
+#     if filename.endswith(".js"):
+#         ######################################################################
+#         # Extraire le nom du composant à partir du nom de fichier
+#         ######################################################################
+#         component_name = re.match(COMPONENT_NAME_REGEX, filename).group(1)
+#         subprocess.run(
+#             f"npx storycap {os.path.join(COMPONENTS_PATH, filename)} --out {os.path.join(STORIES_PATH, component_name)}.stories.js",
+#             shell=True,
+#             check=True
+#         )
+# print("Toutes les stories ont été générées avec succès !")
 
 
 
